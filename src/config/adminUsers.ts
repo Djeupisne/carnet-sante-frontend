329f2296c6fbe08ed242700d195cca0003ef46e6
Djeupisne <oualoumidjeupisne@gmail.com>
2026-02-14 03:01:28 +0000

    Update adminUsers.ts

--- src/config/adminUsers.ts
+++ src/config/adminUsers.ts
@@ -1,1 +1,16 @@
-
+export const ADMIN_USERS = [
+  {
+    email: 'admin@carnetsante.com',
+    password: 'Admin123!',
+    firstName: 'Super',
+    lastName: 'Admin',
+    role: 'admin'
+  },
+  {
+    email: 'superadmin@carnetsante.com',
+    password: 'SuperAdmin123!',
+    firstName: 'Master',
+    lastName: 'Admin',
+    role: 'admin'
+  }
+];
